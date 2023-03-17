0915a956d79dc7b35a3900f470a465d3368e7d1b
Sven <sventajz3@gmail.com>
2023-03-17 18:46:06 +0100

    added color functionality to buttons and added resizing the grid with the range slider

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
 const gridContainer = document.querySelector(".grid-container");
 const slider = document.getElementById("myRange");
 const output = document.querySelector("#slider-output");
-// button selectors
-const blackbtn = document.getElementById("black");
-const rainbowbtn = document.getElementById("rainbow");
-const eraserbtn = document.getElementById("eraser");
-const clearbtn = document.getElementById("clear");
+
+
 output.innerHTML = "16x16";
+// sets the initial color to black see function changeColor for color logic
 let defaultColor = "black";
-let rainbowColor = "rainbow";
-let defaulSize = 16;
+// sets the initial grid size to a 16x16 grid
+let gridSize = 16;
+console.log(defaultColor)
+console.log('default size is ', gridSize)
 
-// function clearGrid(e) {
-//   location.reload()
-// }
-// clearbtn.addEventListener("click", () => {
-//   clear.btnclassList.add("active");
-// });
-
-function currentColor(newColor) {
-  defaultColor = newColor;
-  console.log(defaultColor);
-}
-
-rainbowbtn.addEventListener("click", currentColor(rainbowColor));
-//console.log(defaultColor);
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
@@ -37,38 +23,89 @@ function gridSetup(size) {
     for (j = 0; j < size; j++) {
       const gridItem = document.createElement("div");
       gridItem.classList.add("grid-item");
-      gridItem.addEventListener("click", changeColor);
-      // gridItem.addEventListener("mousedown", changeColor);
+      gridItem.addEventListener("mouseover", changeColor);
+      gridItem.addEventListener("mousedown", changeColor);
       gridContainer.appendChild(gridItem);
     }
   }
 }
 
-function changeColor(e) {
-  // if (e.type === "mouseover" && !mouseDown) return;
-  // sets random RGB values for rainbow color
-  // const randomR = Math.floor(Math.random() * 256);
-  // const randomG = Math.floor(Math.random() * 256);
-  // const randomB = Math.floor(Math.random() * 256);
-  // if (defaultColor == "rainbow") {
-  //   e.target.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
-  //}
-  //else
-  e.target.style.backgroundColor = defaultColor;
+function clearGrid() {
+  gridContainer.innerHTML = ' ';
+  gridSetup(gridSize);
 }
 
-gridSetup(defaulSize);
 
-// slider value display
-// theres probably a better way to display values but this is good for now
+function currentColor(e) {
+  defaultColor = e;
+  console.log(defaultColor);
+}
+
+
+function changeGridSize(){
+  
+  if (slider.value == "1") {
+    gridSize = "8";
+  } else if (slider.value == "2") {
+    gridSize = "16";
+  } else if (slider.value == "3") {
+    gridSize = "32";
+  } else {
+    gridSize = "64";
+  }
+  console.log('default size is ', gridSize)
+  clearGrid();
+}
+
+// button selectors
+const blackbtn = document.getElementById("black");
+const rainbowbtn = document.getElementById("rainbow");
+const eraserbtn = document.getElementById("eraser");
+const clearbtn = document.getElementById("clear");
+
+
+blackbtn.onclick = () => currentColor('black')
+rainbowbtn.onclick = () => currentColor('rainbow');
+eraserbtn.onclick = () => currentColor('eraser');
+clearbtn.onclick = () => clearGrid();
+
+
+
+
+function changeColor(e) {
+  // this logical statement prevents drawing on the grid if the mouse button is 
+  // not being held down
+  if (e.type === "mouseover" && !mouseDown) return;
+  //sets random RGB values for rainbow color
+  const randomR = Math.floor(Math.random() * 256);
+  const randomG = Math.floor(Math.random() * 256);
+  const randomB = Math.floor(Math.random() * 256);
+  if (defaultColor == "rainbow") {
+    e.target.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
+  }
+  else if (defaultColor == "black"){
+  e.target.style.backgroundColor = '#000000';
+  }
+  else e.target.style.backgroundColor = '#FFFFFF';
+}
+
+gridSetup(gridSize);
+
+// displays the slider values in the DOM
+// also each value calls the changeGridSize function for grid resizing
 slider.oninput = function () {
   if (this.value == "1") {
     output.innerHTML = "8x8";
+    changeGridSize(slider.value)
   } else if (this.value == "2") {
     output.innerHTML = "16x16";
+    changeGridSize(slider.value)
   } else if (this.value == "3") {
     output.innerHTML = "32x32";
+    changeGridSize(slider.value)
   } else {
     output.innerHTML = "64x64";
+    changeGridSize(slider.value)
   }
 };
+
